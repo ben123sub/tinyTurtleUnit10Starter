bab8f8ccf12fde746ee41773bde700cdb82764d4
Mykah Taylor <kahben13@yahoo.com>
2017-03-01 21:22:29 +0000

    my comment

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ forward(50);
 
 
 stamp();
+
+function hexagon(){
+forward(90);
+right(50);
+forward(90);
+right(50);
+forward(90);
+right(50);
+forward(90);
+right(50);
+forward(90);
+right(50);
+forward(90);
+right(50);
+forward(90);
+right(30);
+forward(20);
+}
+
+hexagon()
+function rectangle(){
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+}
+
+
+rectangle()
